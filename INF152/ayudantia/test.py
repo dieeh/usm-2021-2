dfc3e44fd5913568e0d9c83cdf3cc4f2c65d877e
dieeh <diego.paz@hotmail.com>
2021-10-26 20:14:40 -0300

    what

---
+++ INF152/ayudantia/test.py
@@ -0,0 +1,15 @@
+def concatenar(caracter, retorno):
+    retorno = retorno
+
+
+
+
+texto = "compete online design event ratin"
+x = " "
+retorno = ""
+for caracter in texto:
+    if x == " ":
+        concatenar(caracter, retorno)
+        x = caracter
+
+print(retorno)
