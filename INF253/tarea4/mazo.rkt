f9779350505ddfd20aebc73123c6d70a44565c29
dieeh <diego.paz@hotmail.com>
2021-12-06 00:08:31 -0300

    updt

--- INF253/tarea4/mazo.rkt
+++ INF253/tarea4/mazo.rkt
@@ -1,2 +1,16 @@
 #lang scheme
 
+(define (divnum x num)
+    (/ x num))
+
+(define (check numero)
+    (define x
+    (- numero 1))
+    (if (= x 0)
+        #t
+        #f))
+
+(define lista '())
+
+(define (mazo cartas divisor)
+    body)
